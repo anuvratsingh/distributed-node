2d4c6ee6d907dc88b51a78ade316970378dfad35
Anuvrat <anuvrats06@gmail.com>
2021-09-15 01:25:49 +0530

    Chapter1 Done

--- packages/recipe-api/src/index.ts
+++ packages/recipe-api/src/index.ts
@@ -1,3 +1,41 @@
-import {hello} from "shared";
+import fastify from 'fastify';
 
-hello("recipie-api")
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 4000;
+
+console.log(`worker pid=${process.pid}`);
+
+const server = fastify();
+
+server.get('/recipes/:id', async (req: any, res) => {
+  console.log(`worker request pid=${process.pid}`);
+  const id = Number(req.params.id);
+  if (id !== 42) {
+    res.statusCode = 404;
+    return { error: 'not_found' };
+  }
+  return {
+    producer_pid: process.pid,
+    recipe: {
+      id,
+      name: 'Chicken Tikka Masala',
+      steps: 'Into the pot...',
+      ingredients: [
+        {
+          id: 1,
+          name: 'Chicken',
+          quantity: '1 lb',
+        },
+        {
+          id: 2,
+          name: 'Sauce',
+          quantity: '2 cups',
+        },
+      ],
+    },
+  };
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Producer recipe-API running at http://${HOST}:${PORT}`);
+});
--- packages/web-api/src/index.ts
+++ packages/web-api/src/index.ts
@@ -1,3 +1,24 @@
-import { hello } from 'shared';
+import fastify from 'fastify';
+// const fastify = require("fastify")
 
-hello('web-api');
+import fetch from 'node-fetch';
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.TARGET || 'localhost:4000';
+
+const server = fastify();
+
+server.get('/', async () => {
+  const req = await fetch(`http://${TARGET}/recipes/42`);
+  const producer_data = await req.json();
+
+  return {
+    consumer_pid: process.pid,
+    producer_data,
+  };
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Consumer web-API  is running at http://${HOST}:${PORT}/`);
+});
