77a3db2d2aba308bd262dd0020e3b69709d23bfc
Anuvrat <anuvrats06@gmail.com>
2021-09-23 10:49:35 +0530

    tried graphql

--- packages/recipe-api/src/index.ts
+++ packages/recipe-api/src/index.ts
@@ -1,41 +1,99 @@
 import fastify from 'fastify';
 
+import fastifyGQL from 'fastify-gql';
+
+import grpc = require('@grpc/grpc-js');
+import loader = require('@grpc/proto-loader');
+
+import fs = require('fs');
+
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 4000;
 
-console.log(`worker pid=${process.pid}`);
-
 const server = fastify();
+// JSON over HTTP
 
-server.get('/recipes/:id', async (req: any, res) => {
-  console.log(`worker request pid=${process.pid}`);
-  const id = Number(req.params.id);
-  if (id !== 42) {
-    res.statusCode = 404;
-    return { error: 'not_found' };
-  }
-  return {
-    producer_pid: process.pid,
-    recipe: {
-      id,
-      name: 'Chicken Tikka Masala',
-      steps: 'Into the pot...',
-      ingredients: [
-        {
-          id: 1,
-          name: 'Chicken',
-          quantity: '1 lb',
-        },
-        {
-          id: 2,
-          name: 'Sauce',
-          quantity: '2 cups',
-        },
-      ],
+{
+//console.log(`worker pid=${process.pid}`);
+// const server = fastify({
+//   https: {
+//     key: fs.readFileSync(__dirname + '/../tls/producer-private-key.key'),
+//     cert: fs.readFileSync(
+//       __dirname + '/../../shared/tls/producer-certificate.cert'
+//     ),
+//   },
+// });
+
+// server.get('/recipes/:id', async (req: any, res) => {
+//   console.log(`worker request pid=${process.pid}`);
+//   const id = Number(req.params.id);
+//   if (id !== 42) {
+//     res.statusCode = 404;
+//     return { error: 'not_found' };
+//   }
+//   return {
+//     producer_pid: process.pid,
+//     recipe: {
+//       id,
+//       name: 'Chicken Tikka Masala',
+//       steps: 'Into the pot...',
+//       ingredients: [
+//         {
+//           id: 1,
+//           name: 'Chicken',
+//           quantity: '1 lb',
+//         },
+//         {
+//           id: 2,
+//           name: 'Sauce',
+//           quantity: '2 cups',
+//         },
+//       ],
+//     },
+//   };
+// });
+
+// server.listen(PORT, HOST, () => {
+//   console.log(`Producer recipe-API running at https://${HOST}:${PORT}`);
+// });
+}
+
+// GraphQL
+{
+  const graphql = fastifyGQL;
+  const schema = fs
+    .readFileSync(__dirname + '/../../shared/src/graphql-schema.gql')
+    .toString();
+
+  const resolvers = {
+    Query: {
+      pid: () => process.pid,
+      recipe: async (_obj, { id }) => {
+        if (id != 42) throw new Error(`recipe ${id} not found`);
+        return {
+          id,
+          name: 'Chicken Tikka Masala',
+          steps: 'Into the pot...',
+        };
+      },
+    },
+    Recipe: {
+      ingredients: async (obj) => {
+        return obj.id != 42
+          ? []
+          : [
+              { id: 1, name: 'Chicken', quantity: '1 lb' },
+              { id: 2, name: 'Sauce', quantity: '2 cups' },
+            ];
+      },
     },
   };
-});
 
-server.listen(PORT, HOST, () => {
-  console.log(`Producer recipe-API running at http://${HOST}:${PORT}`);
-});
+  server
+    .register(graphql, { schema, resolvers, graphiql: true })
+    .listen(PORT, HOST, () => {
+      console.log(
+        `Producer recipe-graphQL-API running at http://${HOST}:${PORT}/graphql`
+      );
+    });
+}
--- packages/web-api/src/index.ts
+++ packages/web-api/src/index.ts
@@ -1,21 +1,64 @@
+// TLS CERT password - 1234
 import fastify from 'fastify';
 // const fastify = require("fastify")
-
 import fetch from 'node-fetch';
+
+import https = require('https');
+import fs = require('fs');
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET || 'localhost:4000';
 
+// GraphQL request
+const complex_query = `
+query kitchenSink($id:ID) {
+  recipe(id: $id) {
+    id 
+    name
+    ingredients {
+      name
+      quantity
+    }
+  }
+  pid
+}
+`;
+
 const server = fastify();
+const options = {
+  agent: new https.Agent({
+    ca: fs.readFileSync(__dirname + '/../../shared/tls/ca-certificate.cert'),
+  }),
+};
+
+// JSON over HTTP
+// server.get('/', async () => {
+//   const req = await fetch(`https://${TARGET}/recipes/42`, options);
+//   const payload = await req.json();
+
+//   return {
+//     consumer_pid: process.pid,   
+//     producer_data: payload,
+//   };
+// });
+
+// GraphQL
 
 server.get('/', async () => {
-  const req = await fetch(`http://${TARGET}/recipes/42`);
-  const producer_data = await req.json();
-
+  const req = await fetch(`http://${TARGET}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: complex_query,
+      variables: { id: '42' },
+    }),
+  });
   return {
     consumer_pid: process.pid,
-    producer_data,
+    producer_data: await req.json(),
   };
 });
 
