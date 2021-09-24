7b0012a00459f94c036b9656f995e383c7050f54
Anuvrat <anuvrats06@gmail.com>
2021-09-24 21:15:40 +0530

    node cluster not working in TS:type error

--- packages/recipe-api/src/index.ts
+++ packages/recipe-api/src/index.ts
@@ -6,94 +6,150 @@ import grpc = require('@grpc/grpc-js');
 import loader = require('@grpc/proto-loader');
 
 import fs = require('fs');
+import { cpus } from "os";
+import * as cluster from "cluster";
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 4000;
 
-const server = fastify();
 // JSON over HTTP
+// producer-http-basic.js
 
 {
-//console.log(`worker pid=${process.pid}`);
-// const server = fastify({
-//   https: {
-//     key: fs.readFileSync(__dirname + '/../tls/producer-private-key.key'),
-//     cert: fs.readFileSync(
-//       __dirname + '/../../shared/tls/producer-certificate.cert'
-//     ),
-//   },
-// });
+  console.log(`worker pid=${process.pid}`);
+  const server = fastify({
+    https: {
+      key: fs.readFileSync(__dirname + '/../tls/producer-private-key.key'),
+      cert: fs.readFileSync(
+        __dirname + '/../../shared/tls/producer-certificate.cert'
+      ),
+    },
+  });
+  server.get('/recipes/:id', async (req: any, res) => {
+    console.log(`worker request pid=${process.pid}`);
+    const id = Number(req.params.id);
+    if (id !== 42) {
+      res.statusCode = 404;
+      return { error: 'not_found' };
+    }
+    return {
+      producer_pid: process.pid,
+      recipe: {
+        id,
+        name: 'Chicken Tikka Masala',
+        steps: 'Into the JSON over HTTP pot...',
+        ingredients: [
+          {
+            id: 1,
+            name: 'Chicken',
+            quantity: '1 lb',
+          },
+          {
+            id: 2,
+            name: 'Sauce',
+            quantity: '2 cups',
+          },
+        ],
+      },
+    };
+  });
 
-// server.get('/recipes/:id', async (req: any, res) => {
-//   console.log(`worker request pid=${process.pid}`);
-//   const id = Number(req.params.id);
-//   if (id !== 42) {
-//     res.statusCode = 404;
-//     return { error: 'not_found' };
-//   }
-//   return {
-//     producer_pid: process.pid,
-//     recipe: {
-//       id,
-//       name: 'Chicken Tikka Masala',
-//       steps: 'Into the pot...',
-//       ingredients: [
-//         {
-//           id: 1,
-//           name: 'Chicken',
-//           quantity: '1 lb',
-//         },
-//         {
-//           id: 2,
-//           name: 'Sauce',
-//           quantity: '2 cups',
-//         },
-//       ],
-//     },
-//   };
-// });
+  server.listen(PORT, HOST, () => {
+    console.log(`Producer recipe-API running at https://${HOST}:${PORT}`);
+  });
 
-// server.listen(PORT, HOST, () => {
-//   console.log(`Producer recipe-API running at https://${HOST}:${PORT}`);
-// });
+  // node cluster
+  // TYPE ERROR (is primary not defined)
+  // const numCore = cpus().length;
+
+  // if (cluster.default.isPrimary) {
+  //   for (let i = 0; i < numCore; i++) {
+  //     cluster.default.fork();
+  //   }
+  // } else {
+  //   server.listen(PORT, HOST, () => {
+  //     console.log(
+  //       `Producer recipe-API ${process.pid} running at https://${HOST}:${PORT}`
+  //     );
+  //   });
+  // }
 }
 
 // GraphQL
 {
-  const graphql = fastifyGQL;
-  const schema = fs
-    .readFileSync(__dirname + '/../../shared/src/graphql-schema.gql')
-    .toString();
+  //const server = fastify();
+  // const graphql = fastifyGQL;
+  // const schema = fs
+  //   .readFileSync(__dirname + '/../../shared/src/graphql-schema.gql')
+  //   .toString();
+  // const resolvers = {
+  //   Query: {
+  //     pid: () => process.pid,
+  //     recipe: async (_obj, { id }) => {
+  //       if (id != 42) throw new Error(`recipe ${id} not found`);
+  //       return {
+  //         id,
+  //         name: 'Chicken Tikka Masala',
+  //         steps: 'Into the graphQL pot...',
+  //       };
+  //     },
+  //   },
+  //   Recipe: {
+  //     ingredients: async (obj) => {
+  //       return obj.id != 42
+  //         ? []
+  //         : [
+  //             { id: 1, name: 'Chicken', quantity: '1 lb' },
+  //             { id: 2, name: 'Sauce', quantity: '2 cups' },
+  //           ];
+  //     },
+  //   },
+  // };
+  // server
+  //   .register(graphql, { schema, resolvers, graphiql: true })
+  //   .listen(PORT, HOST, () => {
+  //     console.log(
+  //       `Producer recipe-graphQL-API running at http://${HOST}:${PORT}/graphql`
+  //     );
+  //   });
+}
 
-  const resolvers = {
-    Query: {
-      pid: () => process.pid,
-      recipe: async (_obj, { id }) => {
-        if (id != 42) throw new Error(`recipe ${id} not found`);
-        return {
-          id,
-          name: 'Chicken Tikka Masala',
-          steps: 'Into the pot...',
-        };
-      },
-    },
-    Recipe: {
-      ingredients: async (obj) => {
-        return obj.id != 42
-          ? []
-          : [
-              { id: 1, name: 'Chicken', quantity: '1 lb' },
-              { id: 2, name: 'Sauce', quantity: '2 cups' },
-            ];
-      },
-    },
-  };
-
-  server
-    .register(graphql, { schema, resolvers, graphiql: true })
-    .listen(PORT, HOST, () => {
-      console.log(
-        `Producer recipe-graphQL-API running at http://${HOST}:${PORT}/graphql`
-      );
-    });
+// gRPC
+{
+  // const pkg_def = loader.loadSync(__dirname + '/../../shared/src/gRPC.proto');
+  // const recipe = grpc.loadPackageDefinition(pkg_def).recipe;
+  // const server = new grpc.Server();
+  // // @ts-ignore
+  // server.addService(recipe.RecipeService.service, {
+  //   getMetaData: (_req, res) => {
+  //     res(null, {
+  //       pid: process.pid,
+  //     });
+  //   },
+  //   getRecipe: (req, res) => {
+  //     if (req.request.id !== 42) {
+  //       return res(new Error(`Unknown recipe ${req.request.id}`));
+  //     }
+  //     res(null, {
+  //       id: 42,
+  //       name: 'Chicke Tikka Masala',
+  //       steps: 'Into the gRPC pot...',
+  //       ingredients: [
+  //         { id: 1, name: 'Chicken', quantity: '1 lb' },
+  //         { id: 2, name: 'Sauce', quantity: '2 cups' },
+  //       ],
+  //     });
+  //   },
+  // });
+  // server.bindAsync(
+  //   `${HOST}:${PORT}`,
+  //   grpc.ServerCredentials.createInsecure(),
+  //   (err, port) => {
+  //     if (err) throw err;
+  //     server.start();
+  //     console.log(
+  //       `Producer recipe-gRPC-API running at http://${HOST}:${port}/`
+  //     );
+  //   }
+  // );
 }
--- packages/web-api/src/index.ts
+++ packages/web-api/src/index.ts
@@ -3,27 +3,17 @@ import fastify from 'fastify';
 // const fastify = require("fastify")
 import fetch from 'node-fetch';
 
+import grpc = require('@grpc/grpc-js');
+import loader = require('@grpc/proto-loader');
+
 import https = require('https');
 import fs = require('fs');
+import util = require('util');
+import path = require('path');
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET || 'localhost:4000';
-
-// GraphQL request
-const complex_query = `
-query kitchenSink($id:ID) {
-  recipe(id: $id) {
-    id 
-    name
-    ingredients {
-      name
-      quantity
-    }
-  }
-  pid
-}
-`;
 
 const server = fastify();
 const options = {
@@ -33,35 +23,84 @@ const options = {
 };
 
 // JSON over HTTP
-// server.get('/', async () => {
-//   const req = await fetch(`https://${TARGET}/recipes/42`, options);
-//   const payload = await req.json();
-
-//   return {
-//     consumer_pid: process.pid,   
-//     producer_data: payload,
-//   };
-// });
+{
+  // server.get('/', async () => {
+  //   const req = await fetch(`https://${TARGET}/recipes/42`, options);
+  //   const payload = await req.json();
+  //   return {
+  //     consumer_pid: process.pid,
+  //     producer_data: payload,
+  //   };
+  // });
+}
 
 // GraphQL
+{
+  //   const complex_query = `
+  // query kitchenSink($id:ID) {
+  //   recipe(id: $id) {
+  //     id
+  //     name
+  //     ingredients {
+  //       name
+  //       quantity
+  //     }
+  //   }
+  //   pid
+  // }
+  // `;
+  //   server.get('/', async () => {
+  //   const req = await fetch(`http://${TARGET}/graphql`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       query: complex_query,
+  //       variables: { id: '42' },
+  //     }),
+  //   });
+  //   return {
+  //     consumer_pid: process.pid,
+  //     producer_data: await req.json(),
+  //   };
+  // });
+  // server.listen(PORT, HOST, () => {
+  //   console.log(`Consumer web-API  is running at http://${HOST}:${PORT}/`);
+  // });
+}
+
+// gRPC
+
+const pkg_def = loader.loadSync(
+  path.join(__dirname, '/../../shared/src/gRPC.proto')
+);
+
+const recipe = grpc.loadPackageDefinition(pkg_def).recipe;
+// @ts-ignore
+const client = new recipe.RecipeService(
+  TARGET,
+  grpc.credentials.createInsecure()
+);
+const getMetaData = util.promisify(client.getMetaData.bind(client));
+const getRecipe = util.promisify(client.getRecipe.bind(client));
 
 server.get('/', async () => {
-  const req = await fetch(`http://${TARGET}/graphql`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: complex_query,
-      variables: { id: '42' },
-    }),
-  });
+  const [meta, recipe] = await Promise.all([
+    getMetaData({}),
+    // @ts-ignore
+    getRecipe({ id: 42 }),
+  ]);
+
   return {
     consumer_pid: process.pid,
-    producer_data: await req.json(),
+    producer_data: meta,
+    recipe,
   };
 });
 
 server.listen(PORT, HOST, () => {
-  console.log(`Consumer web-API  is running at http://${HOST}:${PORT}/`);
+  console.log(`Conusmer web-gRPC-API running at http://${HOST}:${PORT}/`);
 });
+
+path.join(__dirname, '/../../shared/src/gRPC.proto');
